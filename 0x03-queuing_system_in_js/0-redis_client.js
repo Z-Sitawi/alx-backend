d040b8c98511186bfa563359fd7684b49cb04e93
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-07-29 15:34:26 +0100

    set up environment

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+import redis from 'redis';
+// 
+
+const client = redis.createClient({url: 'redis://localhost:6379'});
+
+client.on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err.message}`); 
+});
+
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+client.connect();
