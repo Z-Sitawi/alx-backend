eda44b08fd505ce62e63c21586889ff05ca175d0
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-07-29 16:33:59 +0100

    task 2

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -1,1 +1,32 @@
 #!/usr/bin/node
+import redis from 'redis';
+
+//* Default Connection Behavior 'redis://localhost:6379'
+const client = redis.createClient({ url: 'redis://localhost:6379' });
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+const setNewSchool = (schoolName, value) => {
+  /**
+    * Sets a value for the key `schoolName` in Redis and displays a confirmation message.
+    * @param {string} schoolName - The key to set in Redis.
+    * @param {string} value - The value to set for the key.
+    */
+  client.SET(schoolName, value, redis.print);
+};
+
+const displaySchoolValue = (schoolName) => {
+  client.GET(schoolName, (_err, reply) => {
+    console.log(reply);
+  });
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
