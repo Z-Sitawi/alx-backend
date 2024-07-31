bde341773ba7575105c8b4d7a00e57948439020e
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-07-31 20:01:17 +0100

    task 4

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -1,1 +1,29 @@
-#!/usr/bin/node
+#!/usr/bin/yarn dev
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+// Store values using hset
+client.hset('HolbertonSchools', 'Portland', 50, redis.print);
+client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
+client.hset('HolbertonSchools', 'New York', 20, redis.print);
+client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
+client.hset('HolbertonSchools', 'Cali', 40, redis.print);
+client.hset('HolbertonSchools', 'Paris', 2, redis.print);
+
+const printHash = (hashName) => {
+  client.HGETALL(hashName, (_err, reply) => console.log(reply));
+};
+
+function main() {
+  printHash('HolbertonSchools');
+}
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+  main();
+});
